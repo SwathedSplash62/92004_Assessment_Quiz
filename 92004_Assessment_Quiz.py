4230f402ed8428a42a8b6a2467f4ad25a4b89af0
SwathedSplash62 <138179868+SwathedSplash62@users.noreply.github.com>
2024-03-25 14:59:52 +1300

    Still need to fix the wrong answer return for perm

--- 92004_Assessment_Quiz.py
+++ 92004_Assessment_Quiz.py
@@ -35,6 +35,36 @@ def num_checker(question_num):
                 print(error)
 
 
+def num_questions_wanted(questions_desired):
+    valid = False
+    while not valid:
+        error = "Please enter an integer that is 1 or more"
+
+        while True:
+            to_check = input(questions_desired).lower()
+
+            if to_check == "":
+                return ""
+
+            try:
+
+                # ask user to enter a number
+                response = int(to_check)
+
+                # checks number is more than one
+                if response < 1:
+                    print(error)
+
+                elif to_check == "":
+                    return ""
+                # Outputs error if input is invalid
+                else:
+                    return response
+
+            except ValueError:
+                print(error)
+
+
 # used for simple commands - yes_no - and is easy to implement
 def string_checker(question_string, valid_ans):
     error = f"Please enter a valid option from the following list: {valid_ans}"
@@ -74,14 +104,14 @@ def statement_generator(text, decoration):
 def instructions():
     statement_generator("Instructions/information", "-")
     print('''
-To begin, choose the number of rounds (or press <enter> for infinite mode).
+To begin, choose the number of questions (or press <enter> for infinite mode).
 
 You will receive questions about the area of different shapes, the units will be automatically accounted for; e.g you 
 will not have to enter them 
 
-You may press <enter> to skip a question 
+You may press <enter> to skip a question but it will be marked as wrong 
 
-Type: "quit" to exit the game at anytime 
+Type: "quit" to exit the quiz at anytime 
  
 🐥🐥🐥Good Luck🐥🐥🐥
 ''')
@@ -89,25 +119,17 @@ Type: "quit" to exit the game at anytime
     return ""
 
 
-def sumOfList(list_sum, size):
-    if size == 0:
-        return 0
-    else:
-        return list_sum[size - 1] + sumOfList(list, size - 1)
-
-
 # Main routine goes here
 
-# what the game is supposed to be
+# what the quiz is supposed to be
+end_quiz = "false"
 correct_answers = 0
 total_questions = 0
 mode = "regular"
-rounds_played = 0
+questions_asked = 0
 feedback = ""
-end_game = "no"
-guess = 0
 yes_no_list = ['yes', 'no']
-game_history = []
+quiz_history = []
 all_scores = []
 # Title
 statement_generator("Super duper hard math test", "🤖💎")
@@ -117,86 +139,128 @@ if want_instructions == "yes":
     instructions()
 
 # how many questions they would like to be asked of them?
-num_rounds = num_checker("How many rounds would you like? Push <enter> for ♾️infinite mode♾️: ")
+num_questions = num_questions_wanted("How many questions would you like? Push <enter> for ♾️infinite mode♾️: ")
 
-if num_rounds == "":
+if num_questions == "":
     mode = "infinite"
-    num_rounds = 5
+    num_questions = 5
 
-while rounds_played < num_rounds:
+while questions_asked < num_questions:
 
     # creates infinite
     if mode == "infinite":
-        num_rounds += 1
-
-    # rounds based on mode
+        num_questions += 1
+    if end_quiz == "True":
+        break
+    # type of questioning based on mode
     if mode == "infinite":
-        rounds_heading = f"\n♾️♾️♾️ Round {rounds_played + 1} (Infinite Mode) ♾️♾️♾️"
+        question_heading = f"\n♾️♾️♾️ Round {questions_asked + 1} (Infinite Mode) ♾️♾️♾️"
     else:
-        rounds_heading = f"\n🎉🎉🎉 Round {rounds_played + 1} of {num_rounds} 🎉🎉🎉"
+        question_heading = f"\n🎉🎉🎉 Round {questions_asked + 1} of {num_questions} 🎉🎉🎉"
 
-    print(rounds_heading)
+    print(question_heading)
 
+    # generates the numerical values to be used in the questions
     width = random.randint(1, 20)
     length = random.randint(1, 20)
+    question_type = random.randint(1, 2)
     if length == width:
         shape = "square"
     else:
         shape = "rectangle"
 
-    question = num_checker(f"What is the area of this {shape} if the width is {width}m and length is"
-                           f" {length}m? ")
+    if question_type == 1:
+        question_area = num_checker(f"What is the area of this {shape} if the width is {width}m and length is"
+                                    f" {length}m^2? ")
+        while question_area == "quit" and questions_asked < 1:
+            print("Cannot leave quiz on question 1")
+            question_area = num_checker(f"What is the area of this {shape} if the width is {width}m and length is"
+                                        f" {length}m^2? ")
+        answer = width * length
 
-    if question == "quit":
-        break
-    answer = width * length
-    if question == "":
-        print("You have skipped the question")
-    else:
-        print(f"You chose {question}m")
-    the_right_choice = f"The answer was {answer}m"
-    print(the_right_choice)
-    game_history.append(the_right_choice)
+        if question_area == "":
+            print("You have skipped the question")
+        else:
+            print(f"You chose {question_area}m")
+        the_right_choice = f"{length} x {width} which is {answer}m^2. You were correct"
+        quiz_history.append(the_right_choice)
 
-    if question == answer:
-        result = "Congrats you are right"
-        print(result)
-        rounds_played += 1
-        correct_answers += 1
-        total_questions += 1
-        game_history.append(result)
-    else:
-        result = "You are wrong"
-        print(result)
-        rounds_played += 1
-        total_questions += 1
-        game_history.append(result)
+        if question_area == answer:
+            result = "Congrats you are right"
+            print(result)
+            questions_asked += 1
+            correct_answers += 1
+            total_questions += 1
+            quiz_history.append(result)
+        else:
+            public_result = f"Sorry, you are incorrect. If the length is {length} and the width is {width}" \
+                            f" , the area is " \
+                            f"{length} x {width} which is {answer}m^2"
+            print(public_result)
+            result = f"{length} x {width} = {answer}m. However you said {question_area} "
+            questions_asked += 1
+            total_questions += 1
+            quiz_history.append(result)
 
-if rounds_played > 0:
+    if question_type == 2:
+        question_perimeter = num_checker(
+            f"What is the perimeter of this {shape} if the width is {width}m and length is"
+            f" {length}m? ")
+        while question_perimeter == "quit" and questions_asked < 1:
+            print("Cannot leave quiz on question 1")
+            question_area = num_checker(f"What is the area of this {shape} if the width is {width}m and length is"
+                                        f" {length}m? ")
+            answer = (width * 2) + (length * 2)
 
-    # stats of the results including the average and total as well as game history
+            if question_perimeter == "":
+                print("You have skipped the question")
+            else:
+                print(f"You chose {question_perimeter}m")
+            the_right_choice = f"2({length}) + 2({width}) which is {answer}m. You were correct"
+            quiz_history.append(the_right_choice)
+
+            if question_perimeter == answer:
+                result = "Congrats you are right"
+                print(result)
+                questions_asked += 1
+                correct_answers += 1
+                total_questions += 1
+                quiz_history.append(result)
+            else:
+                public_result = f"Sorry, you are incorrect. If the length is {length} and the width is {width}" \
+                                f" , the area is " \
+                                f"2({length}) + 2({width}) which is {answer}"
+                print(public_result)
+                result = f"2({length}) + 2({width})m. However you said {question_perimeter} "
+                questions_asked += 1
+                total_questions += 1
+                quiz_history.append(result)
+
+if num_questions > 1:
+
+    # stats of the results including the average and total as well as quiz history
 
     # Output stats
-    print("📈📈📈 Game Statistics 📈📈📈")
+    print("📈📈📈 Quiz Statistics 📈📈📈")
 
     total = total_questions
     correct_total = correct_answers
 
     print(
         f"Final Score: {correct_total} out of {total}"
-        f" Average: {correct_total / total:.2f}%")
+        f" Average: {correct_total / total:.0%}")
     print()
 
-    see_history = string_checker("Do you want to see your game history? ", yes_no_list)
+    see_history = string_checker("Do you want to see your quiz history? ", yes_no_list)
     if see_history == "yes":
-        for item in game_history:
+        for item in quiz_history:
             print(item)
 
 
 else:
     "..."
 
-# final statement before game end
+# final statement before quiz end
 
 print("   Thank you for playing")
 statement_generator("Super duper hard math test", "🤖💎")
